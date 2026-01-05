71fcbfe509cfc21738386342bda780f3c3bc6099
zhangxiaode <2279323443@qq.com>
2026-01-05 09:38:55 +0800

    更新

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -31,3 +31,5 @@ export const platform_opts = [
 export const doubao_seedream_4_0_250828 = [
  /// qwen-image-plus / wan2.5-t2i-preview / qwen-image-edit-plus / wan2.5-i2i-preview
 ]
+
+export * from "./character"
--- src/views/project/components/newCharacterModal.vue
+++ src/views/project/components/newCharacterModal.vue
@@ -19,37 +19,28 @@
           <n-form-item label="角色类型:" path="type">
             <n-radio-group v-model:value="form.type" name="type">
               <n-space>
-                <n-radio :value="1">人类</n-radio>
-                <n-radio :value="2">动物</n-radio>
+                <n-radio v-for="(item, index) in character_type_options" :key="index" :value="item.value">{{ item.label }}</n-radio>
               </n-space>
             </n-radio-group>
           </n-form-item>
           <n-form-item label="性别:" path="gender">
             <n-radio-group v-model:value="form.gender" name="gender">
               <n-space>
-                <n-radio :value="1">男</n-radio>
-                <n-radio :value="2">女</n-radio>
-                <n-radio :value="3">未知</n-radio>
+                <n-radio v-for="(item, index) in character_gender_options" :key="index" :value="item.value">{{ item.label }}</n-radio>
               </n-space>
             </n-radio-group>
           </n-form-item>
           <n-form-item label="年龄段:" path="people_years_old">
             <n-radio-group v-model:value="form.people_years_old" name="people_years_old">
               <n-space>
-                <n-radio :value="1">幼年</n-radio>
-                <n-radio :value="2">青年</n-radio>
-                <n-radio :value="3">中年</n-radio>
-                <n-radio :value="4">老年</n-radio>
+                <n-radio v-for="(item, index) in character_people_years_old_options" :key="index" :value="item.value">{{ item.label }}</n-radio>
               </n-space>
             </n-radio-group>
           </n-form-item>
           <n-form-item label="体型:" path="people_body">
             <n-radio-group v-model:value="form.people_body" name="people_body">
               <n-space>
-                <n-radio :value="1">胖</n-radio>
-                <n-radio :value="2">瘦</n-radio>
-                <n-radio :value="3">中等</n-radio>
-                <n-radio :value="4">肌肉</n-radio>
+                <n-radio v-for="(item, index) in character_people_body_options" :key="index" :value="item.value">{{ item.label }}</n-radio>
               </n-space>
             </n-radio-group>
           </n-form-item>
@@ -58,15 +49,7 @@
               class="flex-1"
               v-model:value="form.eye_color"
               placeholder="请选择眼睛颜色"
-              :options="[
-                { label: '黑色', value: 'black' },
-                { label: '蓝色', value: 'blue' },
-                { label: '绿色', value: 'green' },
-                { label: '棕色', value: 'brown' },
-                { label: '红色', value: 'red' },
-                { label: '白色', value: 'white' },
-                { label: '紫色', value: 'purple' },
-              ]"
+              :options="character_eye_color_options"
               clearable
             />
           </n-form-item>
@@ -75,20 +58,7 @@
               class="flex-1"
               v-model:value="form.hair_color"
               placeholder="请选择头发颜色"
-              :options="[
-                { label: '黑色', value: 'black' },
-                { label: '白色', value: 'white' },
-                { label: '蓝色', value: 'blue' },
-                { label: '棕色', value: 'brown' },
-                { label: '金色', value: 'gold' },
-                { label: '红色', value: 'red' },
-                { label: '黄色', value: 'yellow' },
-                { label: '绿色', value: 'green' },
-                { label: '紫色', value: 'purple' },
-                { label: '粉色', value: 'pink' },
-                { label: '灰色', value: 'gray' },
-                { label: '铜橙', value: 'orange' },
-              ]"
+              :options="character_hair_color_options"
               clearable
             />
           </n-form-item>
@@ -97,44 +67,82 @@
               class="flex-1"
               v-model:value="form.hair_style"
               placeholder="请选择头发类型"
-              :options="[
-                { label: '短发', value: 1 },
-                { label: '短卷发', value: 2 },
-                { label: '经典底翘', value: 3 },
-                { label: '底纹淡刻', value: 4 },
-                { label: '光头', value: 5 },
-                { label: '底纹深刻', value: 6 },
-                { label: '长发', value: 7 },
-                { label: '马尾辫', value: 8 },
-                { label: '盘发', value: 9 },
-                { label: '齐刘海', value: 10 },
-                { label: '法式辫', value: 11 },
-                { label: '双马尾', value: 12 },
-                { label: '寸头', value: 13 },
-                { label: '中分', value: 14 },
-                { label: '超长发', value: 15 },
-                { label: '大波浪', value: 16 },
-                { label: '侧扫刘海', value: 17 },
-                { label: '眼间头发', value: 18 },
-                { label: '呆毛收拢', value: 19 },
-                { label: '钻头卷', value: 20 },
-                { label: '公主卷', value: 21 },
-                { label: '露额头', value: 22 },
-                { label: '翼状头发', value: 23 },
-              ]"
+              :options="character_hair_style_options"
               clearable
             />
           </n-form-item>
-          <!-- 
-            cloth_color 衣服颜色 黑色 - black, 白色 - white, 蓝色 - blue, 棕色 - brown, 金色 - gold, 红色 - red, 黄色 - yellow, 绿色 - green, 紫色 - purple, 粉色 - pink, 灰色 - gray（英式：grey）, 橙色 - orange
-            cloth_style 衣服类型 1 风衣、2 夹克、3 冲锋衣、4 牛仔外套、5 开衫、6 卫衣、7 皮衣、8 短袖、9 T恤、10 衬衣、11 衬衫、12 毛衣、13 背心、14 羽绒服、15 长裤、16 短裤、17 丝袜、18 长裙、19 短裙、20 牛仔裤、21 西装、22 工装、23 校服、24 中山装、25 军装、26 警服、27 护士服、28 白大褂、29 女仆装、30 泳装、31 睡衣、32 旗袍、33 礼服、34 皮鞋、35 运动鞋、36 高跟鞋、37 平底鞋、38 靴子、39 拖鞋、40 布鞋、41 凉鞋、42 棉鞋
-            animal_type 动物类型 1狐狸、2狗、3狼、4马、5猫、6鸟、7狮子、8老虎、9鹿、10兔子、11熊、12龙、13独角兽、14凤凰、15海豚
-            animal_body 体型 1消瘦 2正常体型 3肥胖
-            animal_years_old 年龄段1幼年 2成年
-            animal_color 动物颜色 黑色 - black, 白色 - white, 蓝色 - blue, 棕色 - brown, 金色 - gold, 红色 - red, 黄色 - yellow, 绿色 - green, 紫色 - purple, 粉色 - pink, 灰色 - gray, 金黄色 - golden yellow, 橙色 - orange
-            novel_id 小说ID
-            user_id 创建用户ID
-            voice_id 关联音色ID -->
+          <n-form-item label="衣服颜色:" path="cloth_color">
+            <n-select
+              class="flex-1"
+              v-model:value="form.cloth_color"
+              placeholder="请选择衣服颜色"
+              :options="character_cloth_color_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="衣服类型:" path="cloth_style">
+            <n-select
+              class="flex-1"
+              v-model:value="form.cloth_style"
+              placeholder="请选择衣服类型"
+              :options="character_cloth_style_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="动物类型:" path="animal_type">
+            <n-select
+              class="flex-1"
+              v-model:value="form.animal_type"
+              placeholder="请选择动物类型"
+              :options="character_animal_type_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="体型:" path="animal_body">
+            <n-select
+              class="flex-1"
+              v-model:value="form.animal_body"
+              placeholder="请选择体型"
+              :options="character_animal_body_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="年龄段:" path="animal_years_old">
+            <n-select
+              class="flex-1"
+              v-model:value="form.animal_years_old"
+              placeholder="请选择年龄段"
+              :options="character_animal_years_old_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="动物颜色:" path="animal_color">
+            <n-select
+              class="flex-1"
+              v-model:value="form.animal_color"
+              placeholder="请选择动物颜色"
+              :options="character_animal_color_options"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="小说ID:" path="novel_id">
+            <n-select
+              class="flex-1"
+              v-model:value="form.novel_id"
+              placeholder="请选择小说ID"
+              :options="[]"
+              clearable
+            />
+          </n-form-item>
+          <n-form-item label="音色:" path="voice_id">
+            <n-select
+              class="flex-1"
+              v-model:value="form.voice_id"
+              placeholder="请选择关联音色"
+              :options="[]"
+              clearable
+            />
+          </n-form-item>
           <n-form-item label="角色名称:" path="name">
             <n-input v-model:value="form.name" placeholder="请输入角色名称" />
           </n-form-item>
@@ -167,6 +175,7 @@ import { debouncing } from '@/utils/index';
 import { getTemporaryUrl, getSceneDetail, postScene, putScene } from "@/apis/index";
 import CreateSceneModal from './createSceneModal.vue';
 import Upload from '@/components/upload.vue';
+import { character_type_options, character_gender_options, character_people_years_old_options, character_people_body_options, character_eye_color_options, character_hair_color_options, character_hair_style_options, character_cloth_color_options, character_cloth_style_options, character_animal_type_options, character_animal_body_options, character_animal_years_old_options, character_animal_color_options } from '@/constants/index'
 
 const route = useRoute()
 const emit = defineEmits(["save"]);
@@ -179,6 +188,21 @@ const formRef = ref<FormInst | null>(null)
 const uploadRef: any = ref(null)
 const form: any = ref({
   id: null,
+  type: null,
+  gender: null,
+  people_years_old: null,
+  people_body: null,
+  eye_color: '',
+  hair_color: '',
+  hair_style: null,
+  cloth_color: '',
+  cloth_style: null,
+  animal_type: null,
+  animal_body: null,
+  animal_years_old: null,
+  animal_color: '',
+  novel_id: '',
+  voice_id: '',
   name: '',
   desc: '',
   resource_path: ''
