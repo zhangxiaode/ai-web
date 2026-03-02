a6b627b6bf269e52c269db44b08e70b936787767
zhangxiaode <2279323443@qq.com>
2026-03-02 17:35:45 +0800

    优化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="home">
+  <div class="home h-full min-h-600px w-full flex-col-center">
+    <img class="animate-float max-h-379px max-w-392px" src="@/assets/bd-figure.png" />
+    <p class="mt-32px text-26px c-#f2f2f2">欢迎进入丞尧智芯 🎉</p>
+    <p class="mt-16px text-18px c-#eee">您可以通过顶部导航栏进入相应的模块</p>
   </div>
 </template>
 <script lang="ts" setup>
